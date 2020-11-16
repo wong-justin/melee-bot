976168c50961f74da8f673e5dcf7987242f01748
Justin Wong <jkwongfl@yahoo.com>
2020-11-16 18:14:09 -0500

    demo comments

--- demo/demo.py
+++ demo/demo.py
@@ -8,9 +8,9 @@ from livemelee.inputs import *
 # start_game((Bot(), Bot(), None, None))    # two dummy bots will sit in game
 
 # longer demo:
-teabag = [(down,), *wait(5), (center,), *wait(5)]
-toxic_sequence = [      # create a sequence of controller inputs
-    *repeat(2, *teabag),
+teabag = [(down,), *wait(5), (center,), *wait(5)]   # down, center are buttons
+toxic_sequence = [          # create another sequence of controller inputs
+    *repeat(2, *teabag),    # unpack sequences in other sequences
     *taunt(),
     *wait(120)
 ]
@@ -24,18 +24,18 @@ class ReformBot(CheckBot):
         self.commands = {           # call these functions midgame by keypress
             'c': (lambda: _Controller(self.controller.current), # logging func
                   'inspect controller'),                        # descrip
-            'reform': self.stop,    # another cmd: func, no descrip is ok
+            'reform': self.stop,    # another cmd: func - no descrip is ok
         }
         self.set_timer(n=150,
                        do=lambda: self.perform(toxic_sequence),
                        repeat=True)
-    def stop(self):
+    def stop(self):                 # replace toxic action with inaction
         self.do = lambda:None
 
 dummy = Bot()
 bot = ReformBot()
 
 live_interface = LiveGameStats(commands=bot.commands)   # init live thread
-                                                        # adding our new cmds
+                                                        #  adding our new cmds
 start_game((dummy, bot, None, None),        # use ports 1 and 2
-           live_interface=live_interface)   # dont forget to pass custom obj
+           live_interface=live_interface)   # dont forget to pass that obj
